290ee0c93facf235e9b0e98d8545db4cbf420e4c
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-09-30 13:58:16 +0200

    https://github.com/ubtue/DatenProbleme/issues/1164

--- zotero_master/translators/PicaSWBixzs.js
+++ zotero_master/translators/PicaSWBixzs.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 2,
 	"browserSupport": "gcs",
-	"lastUpdated": "2020-09-15 13:54:00"
+	"lastUpdated": "2020-09-30 13:54:00"
 }
 
 
@@ -502,9 +502,9 @@ function performExport() {
                     processDocumentsCustom(lookupUrl,
                         // processing callback function
                         function(doc, url, threadParams){
-                            var ppn = Zotero.Utilities.xpathText(doc, '//tr[(((count(preceding-sibling::*) + 1) = 5) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "presvalue", " " ))]//div');
+                            var ppn = Zotero.Utilities.xpathText(doc, '//div[a[img]]'); //Z.write(ppn)
                             if (ppn) {
-                                var authorValue = "!" + ppn.match(/\d+X?/) + "!$BVerfasserIn$4aut \n8910 $aixzom$bAutor in der Zoterovorlage ["  + threadParams["authorName"] + "] maschinell zugeordnet";
+                                var authorValue = "!" + ppn.match(/^\d+X?/) + "!$BVerfasserIn$4aut \n8910 $aixzom$bAutor in der Zoterovorlage ["  + threadParams["authorName"] + "] maschinell zugeordnet";
                                 addLine(threadParams["currentItemId"], threadParams["code"], authorValue);
                             } else {
                                 addLine(threadParams["currentItemId"], threadParams["code"], threadParams["authorName"]  + "$BVerfasserIn$4aut");
