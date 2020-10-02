2fbbaed6fccb1474620c4e94af56919af3a98188
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-10-02 11:56:17 +0200

    https://github.com/ubtue/DatenProbleme/issues/1101

--- zotero_master/translators/Springer Link.js
+++ zotero_master/translators/Springer Link.js
@@ -9,8 +9,26 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsbv",
-	"lastUpdated": "2020-09-18 08:57:35"
+	"lastUpdated": "2020-10-02 11:47:25"
 }
+
+/*
+   SpringerLink Translator
+   Copyright (C) 2020 Aurimas Vinckevicius and Sebastian Karcher
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 function detectWeb(doc, url) {
 	var action = url.match(/^https?:\/\/[^/]+\/([^/?#]+)/);
@@ -59,6 +77,9 @@ function getResultList(doc) {
 	}
 	if (!results.length) {
 		results = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "c-card__title", " " ))]//a');
+	}
+	if (!results.length) {
+		results = ZU.xpath(doc, '//li[@class="c-list-group__item"]//h3/a');
 	}
 	return results;
 }
