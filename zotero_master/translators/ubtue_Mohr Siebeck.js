7d569d3908e9decf98b3891132f46ab4bfe67327
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-10-02 13:28:01 +0200

    https://github.com/ubtue/DatenProbleme/issues/1131

--- zotero_master/translators/ubtue_Mohr Siebeck.js
+++ zotero_master/translators/ubtue_Mohr Siebeck.js
@@ -1,15 +1,15 @@
 {
 	"translatorID": "30f0052d-e8fc-45ac-a1db-7a729f0da376",
-	"label": "Mohr Siebeck",
+	"label": "ubtue_Mohr Siebeck",
 	"creator": "Madeesh Kannan",
 	"target": "https?://www.mohrsiebeck.com/",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 90,
-	"inRepository": false,
+	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2019-05-17 14:20:25"
+	"lastUpdated": "2020-10-02 13:25:59"
 }
 
 /*
@@ -63,7 +63,7 @@ function postProcess(doc, item) {
 
 	item.tags = ZU.xpath(doc, '//div[@id="productKeywords"]//a').map(i => i.textContent.trim());
 
-	item.creators = ZU.xpathText(doc, '//h2[contains(@class, "product-heading-author-block")]').split(",").map(i => ZU.cleanAuthor(i));
+	if (item.creators.length) item.creators = ZU.xpathText(doc, '//h2[contains(@class, "product-heading-author-block")]').split(",").map(i => ZU.cleanAuthor(i));
 
 	if (!item.language)
 		item.language = ZU.xpathText(doc, '//meta[@name="language"]/@content');
