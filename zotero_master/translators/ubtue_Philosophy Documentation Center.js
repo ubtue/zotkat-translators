ecf1b0e0db886be81cf2544f9f519f4c99f9344b
Kim, Timotheus Chang-whae <kim.changhe@googlemail.com>
2020-10-20 17:48:18 +0200

    https://github.com/ubtue/DatenProbleme/issues/1179

--- zotero_master/translators/ubtue_Philosophy Documentation Center.js
+++ zotero_master/translators/ubtue_Philosophy Documentation Center.js
@@ -6,10 +6,10 @@
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
-	"inRepository": true,
+	"inRepository": false,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-05-14 20:58:26"
+	"lastUpdated": "2020-10-20 17:26:12"
 }
 
 /*
@@ -35,10 +35,10 @@
 
 function detectWeb(doc, url) {
   if (url.includes('/jrv/')) {
-    return "JournalArticle";
+	return "JournalArticle";
   } else
   if (url.includes('/collection-anonymous/')) {
-    return "multiple";
+	return "multiple";
   }
   return false;
 }
@@ -46,10 +46,11 @@ function detectWeb(doc, url) {
 function getSearchResults(doc) {
 	var items = {};
 	var found = false;
-	var rows = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "purchase", " " ))]')
+	var rows = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "purchase", " " ))]');
 	for (let i=0; i<rows.length; i++) {
 		let href = rows[i].href;
-		let title = ZU.trimInternal(rows[i].textContent);
+		let titles = ZU.trimInternal(rows[i].outerHTML);
+		let title = titles.match(/Item_Title=.*&/)[0].split(';')[0].replace(/Item_Title=/, ''); //Z.debug(title)
 		if (!href || !title) continue;
 		found = true;
 		items[href] = title;
@@ -59,12 +60,12 @@ function getSearchResults(doc) {
 
 function doWeb(doc, url) {
   if (detectWeb(doc, url) == "multiple") {
-    Zotero.selectItems(getSearchResults(doc, false), function (items) {
-      if (items) ZU.processDocuments(Object.keys(items), scrape);
-    });
+	Zotero.selectItems(getSearchResults(doc, false), function (items) {
+	  if (items) ZU.processDocuments(Object.keys(items), scrape);
+	});
   } else
   {
-    scrape(doc, url);
+	scrape(doc, url);
   }
 }
 
@@ -74,17 +75,63 @@ function scrape(doc) {
 	translator.setDocument(doc);
 	translator.setHandler("itemDone", function (t, i) {
 		i.itemType = "journalArticle";
-		
-		postProcess(doc, i);
+		if (i.publicationTitle = 'Philotheos') i.ISSN = '2620-0163';
+		i.complete();
+		//postProcess(doc, i);
 	});
 	translator.translate();
 }
 
-function postProcess(doc, item) {
-	/*let tagentry = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "toggleAbstract", " " ))]')
+/*function postProcess(doc, item) {
+	let tagentry = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "toggleAbstract", " " ))]')
 	if (tagentry) {
 		item.tags.push("Book Reviews");
 		
-	}*/
+	}
 		item.complete();
-}
+}*/
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://www.pdcnet.org/philotheos/content/philotheos_2019_0019_0002_0149_0165",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Investigating the terms of transition from a dialogue to dialectics in Plato’s Charmides",
+				"creators": [
+					{
+						"firstName": "Christos",
+						"lastName": "Terezis",
+						"creatorType": "author"
+					}
+				],
+				"date": "2019/10/01",
+				"DOI": "10.5840/philotheos20191928",
+				"ISSN": "2620-0163",
+				"abstractNote": "In this article, following the introductory chapters of the Platonic dialogue Charmides (153a1-154b7), we attempt to investigate the terms of transition from a simple dialogue to dialectics. Interpreting the expressive means used, we attempt to explain how Plato goes from historicity to systematicity, in order to create the appropriate conditions to build a definition about a fundamental virtue as well as to set the criteria to be followed in a philosophical debate. Our study is divided in two sections, each of which is also divided in two subsections. In the first section, we investigate the historical context of the dialogue and the terms of transition from a single dialogue to dialectics. In the second section, we attempt to define according to Socrates’ judgments the mental and moral quality of the young men as well as the terms and conditions of the right interlocutor. At the end of each section, we present a table of concepts to bring to light the conceptual structures that Plato builds, which reveal the philosophical development in this dialogue.",
+				"extra": "DOI: 10.5840/philotheos20191928",
+				"issue": "2",
+				"libraryCatalog": "www.pdcnet.org",
+				"pages": "149-165",
+				"publicationTitle": "Philotheos",
+				"url": "https://www.pdcnet.org/pdc/bvdb.nsf/purchase?openform&fp=philotheos&id=philotheos_2019_0019_0002_0149_0165",
+				"volume": "19",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
