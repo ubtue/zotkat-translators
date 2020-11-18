b265661e1175b3f4bf6e9a9fbe007467a8177802
socheres <kim.changhe@googlemail.com>
2020-11-18 15:56:49 +0100

    fix errors

--- zotero_des/PicaSWB_batch.js
+++ zotero_des/PicaSWB_batch.js
@@ -232,7 +232,7 @@ function addLine(itemid, code, value) {
 	value = EscapeNonASCIICharacters(value);
 
     //Zeile zusammensetzen
-    var line = code + " " + value.trim().replace(/"/g, '\\"').replace(/“/g, '\\"').replace(/”/g, '\\"').replace(/„/g, '\\"').replace('|s|RezensionstagPica', '').replace(/\t/g, '').replace('|s|Book Reviews', '|f|Book Reviews').replace(/\t/g, '');
+    var line = code + " " + value.trim().replace(/"/g, '\\"').replace(/“/g, '\\"').replace(/”/g, '\\"').replace(/„/g, '\\"').replace('|s|RezensionstagPica', '').replace(/\t/g, '').replace(/\|s\|book\s+reviews?)i', '|f|Book Reviews').replace(/\t/g, '').replace(/\|s\|peer\s?reviewed?/i, '|f|Peer reviewed');
     itemsOutputCache[itemid].push(line);
 }
 
