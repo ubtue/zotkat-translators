41df4caa82487cdbe43a5dff07517b409eb542d5
socheres <kim.changhe@googlemail.com>
2021-02-26 11:30:35 +0100

    https://github.com/ubtue/DatenProbleme/issues/1414

--- zotero_master/translators/ubtue_Brill.js
+++ zotero_master/translators/ubtue_Brill.js
@@ -9,7 +9,7 @@
 	"inRepository": false,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-		"lastUpdated": "2020-09-25 16:23:38"
+	"lastUpdated": "2020-02-26 11:24:25"
 }
 
 /*
@@ -44,9 +44,15 @@ function detectWeb(doc, url) {
 
 function getSearchResults(doc) {
 	let items = {};
-    let found = false;
-	let links = ZU.xpath(doc, '//a[contains(@class, "c-Typography--title") | //*[contains(concat( " ", @class, " " ), concat( " ", "label", " " ))]');Z.debug(links)
-	let text = ZU.xpath(doc, '//a[contains(@class, "c-Typography--title")]/span')
+	let found = false;
+	let links = doc.querySelectorAll(".c-Typography--title");
+	let usesTypography = !!links.length;
+	if (!usesTypography) {
+		links = doc.querySelectorAll(".c-Button--link, [target='_self']");
+	}
+	let text = usesTypography ?
+		    doc.querySelectorAll(".c-Typography--title > span") :
+		    doc.querySelectorAll(".c-Button--link, [target='_self']");
 	for (let i = 0; i < links.length; ++i) {
 		let href = links[i].href;
 		let title = ZU.trimInternal(text[i].textContent);
@@ -62,11 +68,21 @@ function getSearchResults(doc) {
 
 function postProcess(doc, item) {
 	if (!item.abstractNote) {
-        item.abstractNote = ZU.xpath(doc, '//section[@class="abstract"]//p');
-        if (item.abstractNote && item.abstractNote.length > 0)
-            item.abstractNote = item.abstractNote[0].textContent.trim();
+	  item.abstractNote = ZU.xpath(doc, '//section[@class="abstract"]//p');
+	  if (item.abstractNote && item.abstractNote.length > 0)
+	     item.abstractNote = item.abstractNote[0].textContent.trim();
+	  else
+	     item.abstractNote = '';
     }
-	if (item.abstractNote === undefined) item.abstractNote = '';
+	item.tags = ZU.xpath(doc, '//dd[contains(@class, "keywords")]//a');
+	if (item.tags)
+		item.tags = item.tags.map(i => i.textContent.trim());
+	let reviewEntry = text(doc, '.articlecategory');
+	if (reviewEntry && reviewEntry.match(/book\sreview/i)) item.tags.push('Book Review');
+	if (item.issue) item.issue = item.issue.replace('-', '/');
+	if (!item.itemType)
+            item.itemType = "journalArticle";
+    
 	// i set 100 as limit of string length, because usually a string of a pseudoabstract has less then 150 character e.g. "abstractNote": "\"Die Vernünftigkeit des jüdischen Dogmas\" published on 05 Sep 2020 by Brill."
 	if (item.abstractNote.length < 100) delete item.abstractNote;	
 	item.tags = ZU.xpath(doc, '//dd[contains(@class, "keywords")]//a');
@@ -98,126 +114,5 @@ function doWeb(doc, url) {
 			ZU.processDocuments(articles, invokeEmbeddedMetadataTranslator);
 		});
 	} else
-        invokeEmbeddedMetadataTranslator(doc, url);
+		invokeEmbeddedMetadataTranslator(doc, url);
 }
-
-
-/** BEGIN TEST CASES **/
-var testCases = [
-	{
-		"type": "web",
-		"url": "https://www.mohrsiebeck.com/artikel/machtkonstellationen-jenseits-von-realismus-und-idealismus-101628003181516x14791276269803",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"title": "Machtkonstellationen: Jenseits von Realismus und Idealismus",
-				"creators": [
-					{
-						"firstName": "Georg",
-						"lastName": "Zenkert"
-					}
-				],
-				"date": "2016",
-				"DOI": "10.1628/003181516X14791276269803",
-				"ISSN": "0031-8159",
-				"abstractNote": "Claudia Horst: Marc Aurel. Philosophie und politische Macht zur Zeit der Zweiten Sophistik. Franz Steiner Verlag. Stuttgart 2013. 232 S. Herfried Münkler/Rüdiger Voigt/Ralf Walkenhaus (Hg.): Demaskierung der Macht. Niccolò Machiavellis Staats- und Politikverständnis. Nomos. 2. Auflage. Baden-Baden 2013. 224 S. Dietrich Schotte: Die Entmachtung Gottes durch den Leviathan. Thomas Hobbes über die Religion. Frommann-Holzboog. Stuttgart-Bad Cannstatt 2013. 360 S.",
-				"issue": "3",
-				"language": "de",
-				"libraryCatalog": "Mohr Siebeck",
-				"pages": "195-206",
-				"publicationTitle": "Philosophische Rundschau (PhR)",
-				"shortTitle": "Machtkonstellationen",
-				"volume": "63",
-				"attachments": [
-					{}
-				],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "https://brill.com/view/journals/zrgg/72/4/article-p371_2.xml",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"title": "Die Vernünftigkeit des jüdischen Dogmas: Samuel Holdheims Kritik an Mendelssohns Religionsphilosophie",
-				"creators": [
-					{
-						"firstName": "George Y.",
-						"lastName": "Kohler",
-						"creatorType": "author"
-					}
-				],
-				"date": "2020/09/05",
-				"DOI": "10.1163/15700739-07204002",
-				"ISSN": "0044-3441, 1570-0739",
-				"issue": "4",
-				"language": "de",
-				"libraryCatalog": "brill.com",
-				"pages": "371-389",
-				"publicationTitle": "Zeitschrift für Religions- und Geistesgeschichte",
-				"shortTitle": "Die Vernünftigkeit des jüdischen Dogmas",
-				"url": "https://brill.com/view/journals/zrgg/72/4/article-p371_2.xml",
-				"volume": "72",
-				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Snapshot",
-						"mimeType": "text/html"
-					}
-				],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	},
-	{
-		"type": "web",
-		"url": "https://brill.com/view/journals/zrgg/72/4/article-p371_2.xml",
-		"items": [
-			{
-				"itemType": "journalArticle",
-				"title": "Die Vernünftigkeit des jüdischen Dogmas: Samuel Holdheims Kritik an Mendelssohns Religionsphilosophie",
-				"creators": [
-					{
-						"firstName": "George Y.",
-						"lastName": "Kohler",
-						"creatorType": "author"
-					}
-				],
-				"date": "2020/09/05",
-				"DOI": "10.1163/15700739-07204002",
-				"ISSN": "0044-3441, 1570-0739",
-				"issue": "4",
-				"language": "de",
-				"libraryCatalog": "brill.com",
-				"pages": "371-389",
-				"publicationTitle": "Zeitschrift für Religions- und Geistesgeschichte",
-				"shortTitle": "Die Vernünftigkeit des jüdischen Dogmas",
-				"url": "https://brill.com/view/journals/zrgg/72/4/article-p371_2.xml",
-				"volume": "72",
-				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Snapshot",
-						"mimeType": "text/html"
-					}
-				],
-				"tags": [],
-				"notes": [],
-				"seeAlso": []
-			}
-		]
-	}
-]
-/** END TEST CASES **/
