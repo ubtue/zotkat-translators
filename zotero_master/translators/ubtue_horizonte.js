0ac6a7b3aeca50b42499dfb525885fb524efece6
socheres <kim.changhe@googlemail.com>
2021-04-21 17:00:00 +0200

    test

--- zotero_master/translators/ubtue_horizonte.js
+++ zotero_master/translators/ubtue_horizonte.js
@@ -9,9 +9,9 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-10-30 11:08:39"
+	"lastUpdated": "2020-10-30 11:08:44"
 }
-
+q
 /*
 	***** BEGIN LICENSE BLOCK *****
 
