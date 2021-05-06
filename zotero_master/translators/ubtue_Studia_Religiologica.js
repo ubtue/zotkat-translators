83c6c0b8a1aa7c71bcd035d912ba32c8c8710ad2
socheres <kim.changhe@googlemail.com>
2021-05-06 09:08:20 +0200

    new translator for studia religiologica test cases https://github.com/ubtue/DatenProbleme/issues/1496

--- zotero_master/translators/ubtue_Studia_Religiologica.js
+++ zotero_master/translators/ubtue_Studia_Religiologica.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-05-06 06:54:49"
+	"lastUpdated": "2021-05-06 07:08:09"
 }
 
 /*
@@ -35,7 +35,6 @@
 	***** END LICENSE BLOCK *****
 */
 
-
 function detectWeb(doc, url) {
 	if (url.includes('/art/')) return "journalArticle";
 		else if (getSearchResults(doc, true)) return "multiple";
@@ -45,10 +44,10 @@ function detectWeb(doc, url) {
 function getSearchResults(doc, checkOnly) {
   let items = {};
   let found = false;
-  let rows = doc.querySelectorAll('.title');//Z.debug(rows)
+  let rows = doc.querySelectorAll('.title');
   for (let row of rows) {
-	let href = row.href; //Z.debug(href)
-	let title = ZU.trimInternal(row.textContent)//Z.debug(title)
+	let href = row.href;
+	let title = ZU.trimInternal(row.textContent)
 	if (!href || !title) continue;
 	if (checkOnly) return true;
 	found = true;
@@ -56,18 +55,6 @@ function getSearchResults(doc, checkOnly) {
   }
   return found ? items : false;
 }
-
-
-/*function doWeb(doc, url) {
-	if (detectWeb(doc, url) == "multiple") {
-		Zotero.selectItems(getSearchResults(doc, false), function (items) {
-			if (items) ZU.processDocuments(Object.keys(items), scrape);
-		});
-	}
-	else {
-		scrape(doc, url);
-	}
-}*/
 
 function doWeb(doc, url) {
 	if (detectWeb(doc, url) === "multiple") {
@@ -91,10 +78,111 @@ function scrape(doc, url) {
 	translator.setTranslator('951c027d-74ac-47d4-a107-9c3069ab7b48');
 	translator.setDocument(doc);
 	translator.setHandler('itemDone', function (t, item) {
-		let checkLanguage = ZU.xpathText(doc, '(//div[@class="abstract-text"]//div[@class="text input-text"]//p//strong)[1]');Z.debug(testLanguage)	
+		let checkLanguage = ZU.xpathText(doc, '(//div[@class="abstract-text"]//div[@class="text input-text"]//p//strong)[1]');	
 		if (checkLanguage === null) item.language = 'en';	
 			item.complete();
 		});
 	translator.translate();
 }
 
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://www.ejournals.eu/Studia-Religiologica/2020/Numer-53-4-2020/art/18056/",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "God and Difference",
+				"creators": [
+					{
+						"firstName": "Krzysztof",
+						"lastName": "Mech",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020/12/1",
+				"DOI": "10.4467/20844077SR.20.018.13036",
+				"ISSN": "2084-4077",
+				"abstractNote": "God and Difference",
+				"archiveLocation": "World",
+				"issue": "Tom 53, Numer 4",
+				"language": "en",
+				"libraryCatalog": "www.ejournals.eu",
+				"pages": "255-274",
+				"publicationTitle": "Studia Religiologica",
+				"url": "https://www.ejournals.eu/Studia-Religiologica/2020/Numer-53-4-2020/art/18056/",
+				"volume": "2020",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Studia Religiologica"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.ejournals.eu/Studia-Religiologica/2020/Numer-53-4-2020/art/18055/",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Postawa męstwa bycia – odpowiedź dana nie tylko Hamletowi",
+				"creators": [
+					{
+						"firstName": "Marcin",
+						"lastName": "Hintz",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Maria",
+						"lastName": "Urbańska-Bożek",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020/12/1",
+				"DOI": "10.4467/20844077SR.20.019.13037",
+				"ISSN": "2084-4077",
+				"abstractNote": "Postawa męstwa bycia – odpowiedź dana nie tylko Hamletowi",
+				"archiveLocation": "World",
+				"issue": "Tom 53, Numer 4",
+				"language": "pl",
+				"libraryCatalog": "www.ejournals.eu",
+				"pages": "275-287",
+				"publicationTitle": "Studia Religiologica",
+				"url": "https://www.ejournals.eu/Studia-Religiologica/2020/Numer-53-4-2020/art/18055/",
+				"volume": "2020",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Studia Religiologica"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
