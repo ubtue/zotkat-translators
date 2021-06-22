80a89edfb946699e535192ce6d6c08ae8ebbf564
socheres <kim.changhe@googlemail.com>
2021-06-22 15:37:51 +0200

    delete ixrk https://github.com/ubtue/DatenProbleme/issues/1508

--- zotero_master/translators/Selektiv_PicaSWB_batch.js
+++ zotero_master/translators/Selektiv_PicaSWB_batch.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 2,
 	"browserSupport": "gcs",
-	"lastUpdated": "2021-04-26 13:39:00"
+	"lastUpdated": "2021-06-22 15:35:00"
 }
 
 // Zotero Export Translator für das Pica Intern Format
@@ -724,7 +724,7 @@ function performExport() {
 				}
 			}
 
-			addLine(currentItemId, '\\nE* l01\\n7100$Jn\\n8012 ixzs$aixzo$aixrk");\napplication.activeWindow.pressButton("Enter");\n\n', ""); //K10plus:das "j" in 7100 $jn wird jetzt groß geschrieben, also $Jn / aus 8002,  dem Feld für die lokalen Abrufzeichen, wird 8012/ 8012 mehrere Abrufzeichen werden durch $a getrennt, nicht wie bisher durch Semikolon. Also: 8012 ixzs$aixzo$ixrk
+			addLine(currentItemId, '\\nE* l01\\n7100$Jn\\n8012 ixzs$aixzo");\napplication.activeWindow.pressButton("Enter");\n\n', ""); //K10plus:das "j" in 7100 $jn wird jetzt groß geschrieben, also $Jn / aus 8002,  dem Feld für die lokalen Abrufzeichen, wird 8012/ 8012 mehrere Abrufzeichen werden durch $a getrennt, nicht wie bisher durch Semikolon. Also: 8012 ixzs$aixzo
         }
     }
 
