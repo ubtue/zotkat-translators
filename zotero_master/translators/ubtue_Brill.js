afefcc7b9d51608cf147a8efbdd2dd8291dc500c
socheres <kim.changhe@googlemail.com>
2021-09-08 11:06:59 +0200

    berichtsjahr brill https://github.com/ubtue/DatenProbleme/issues/1583

--- zotero_master/translators/ubtue_Brill.js
+++ zotero_master/translators/ubtue_Brill.js
@@ -6,10 +6,10 @@
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 90,
-	"inRepository": false,
+	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2020-03-29 15:34:25"
+	"lastUpdated": "2021-09-08 09:05:41"
 }
 
 /*
@@ -40,6 +40,7 @@ function detectWeb(doc, url) {
 	} else if (url.match(/issue-\d+(-\d+)?\.xml$/)) {
 		return "multiple";
  	}
+	return false;
 }
 
 function getSearchResults(doc) {
@@ -74,33 +75,64 @@ function postProcess(doc, item) {
 	  else
 		 item.abstractNote = '';
 	}
+	// i set 100 as limit of string length, because usually a string of a pseudoabstract has less then 150 character e.g. "abstractNote": "\"Die Vernünftigkeit des jüdischen Dogmas\" published on 05 Sep 2020 by Brill."
+	if (item.abstractNote.length < 100) delete item.abstractNote;
+
 	item.tags = ZU.xpath(doc, '//dd[contains(@class, "keywords")]//a');
-	if (item.tags)
-		item.tags = item.tags.map(i => i.textContent.trim());
+	if (item.tags) {
+		let allTags = item.tags.map(i => i.textContent.trim());
+		//deduplicate allTags
+		item.tags = Array.from(new Set(allTags.map(JSON.stringify))).map(JSON.parse);
+	}
 	let reviewEntry = text(doc, '.articlecategory');
 	if (reviewEntry && reviewEntry.match(/book\sreview/i)) item.tags.push('Book Review');
-	let issue = ZU.xpathText(doc, '//meta[@name="citation_issue"]/@content'); 
-	if (issue) item.issue = issue.replace('-', '/');
-	let volume = ZU.xpathText(doc, '//meta[@name="citation_volume"]/@content');
-	if (volume) item.volume = volume;
-	let pages = ZU.xpathText(doc, '//meta[@name="citation_firstpage"]/@content') + '-' + ZU.xpathText(doc, '//meta[@name="citation_lastpage"]/@content');
-	if (pages) item.pages = pages;
-	let issn = ZU.xpathText(doc, '//meta[@name="citation_issn"]/@content');
-	if (issn) item.ISSN = issn.match(/\d{4}-\d{4}/).toString();
-	let title = ZU.xpathText(doc, '//meta[@name="citation_title"]/@content');
-	if (title) item.title = title;
-	let doi = ZU.xpathText(doc, '//meta[@name="citation_doi"]/@content');
-	if (doi) item.DOI = doi;
-	if (!item.itemType)
-			item.itemType = "journalArticle";
-	
-	// i set 100 as limit of string length, because usually a string of a pseudoabstract has less then 150 character e.g. "abstractNote": "\"Die Vernünftigkeit des jüdischen Dogmas\" published on 05 Sep 2020 by Brill."
-	if (item.abstractNote.length < 100) delete item.abstractNote;	
-	item.tags = ZU.xpath(doc, '//dd[contains(@class, "keywords")]//a');
-	if (item.tags)
-		item.tags = item.tags.map(i => i.textContent.trim());
-	let language = ZU.xpathText(doc, '//meta[@name="citation_language"]/@content');
-	if(language) item.language = language;
+	// numbering issues with slash due to cataloguing rule
+	if (item.issue) item.issue = item.issue.replace('-', '/');
+	let date = item.date;
+	//entry for scraping Berichtsjahr
+	let dateEntry = ZU.xpathText(doc, '//div[@class="cover cover-image configurable-index-card-cover-image"]//@title');
+	let berichtsjahr = extractBerichtsjahr(dateEntry);
+	let erscheinungsjahr = extractErscheinungsjahr(date);
+	if (erscheinungsjahr !== berichtsjahr) {
+		item.date = extractBerichtsjahr(dateEntry);
+	} else {
+		item.date;
+	}
+
+	//scrape ORCID from website
+	let authorSectionEntries = doc.querySelectorAll('.text-subheading span');
+	for (let authorSectionEntry of authorSectionEntries) {
+		let authorInfo = authorSectionEntry.querySelector('.c-Button--link');
+		let orcidHref = authorSectionEntry.querySelector('.orcid');
+		if (authorInfo && orcidHref) {
+			let author = authorInfo.childNodes[0].textContent;
+			let orcid = orcidHref.textContent.replace(/.*(\d{4}-\d+-\d+-\d+x?)$/i, '$1');
+			item.notes.push({note: "orcid:" + orcid + ' | ' + author});
+		}
+	}
+	//delete symbols in names
+	for (let i in item.creators) {
+		item.creators[i].lastName = item.creators[i].lastName.replace('†', '');
+		item.creators[i].firstName = item.creators[i].firstName.replace('†', '');
+	}
+	//deduplicate
+	item.notes = Array.from(new Set(item.notes.map(JSON.stringify))).map(JSON.parse);
+	// mark articles as "LF" (MARC=856 |z|kostenfrei), that are published as open access	
+	let openAccessTag = text(doc, '.has-license span');
+	if (openAccessTag && openAccessTag.match(/open\s+access/gi)) item.notes.push('LF:');
+  // mark articles as "LF" (MARC=856 |z|kostenfrei), that are free accessible e.g. conference report 10.30965/25890433-04902001 
+	let freeAccess = text(doc, '.color-access-free');
+	if (freeAccess && freeAccess.match(/(free|freier)\s+(access|zugang)/gi)) item.notes.push('LF:');
+	if (!item.itemType)	item.itemType = "journalArticle";
+}
+
+function extractErscheinungsjahr(date) {
+	return date ? date.trim().match(/\d{4}/)[0] : '';
+}
+
+function extractBerichtsjahr(dateEntry) {
+	let dateCandidate = dateEntry.match(/\(\s*(\d{4})\s*\):/);
+	return dateCandidate.length > 1 ? dateCandidate[1] : null;
 }
 
 function invokeEmbeddedMetadataTranslator(doc, url) {
@@ -129,3 +161,136 @@ function doWeb(doc, url) {
 	} else
 		invokeEmbeddedMetadataTranslator(doc, url);
 }
+
+	/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "https://brill.com/view/journals/ormo/100/2/ormo.100.issue-2.xml",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://brill.com/view/journals/ormo/100/2/article-p147_2.xml",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "‘Our Traditions Will Kill Us!’: Negotiating Marriage Celebrations in the Face of Legal Regulation of Tradition in Tajikistan",
+				"creators": [
+					{
+						"firstName": "Elena",
+						"lastName": "Borisova",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020",
+				"DOI": "10.1163/22138617-12340248",
+				"ISSN": "2213-8617, 0030-5472",
+				"abstractNote": "Abstract Based on extensive ethnographic research in northern Tajikistan, this article examines the implications of the law ordering traditions and rituals (tanzim), including marriage celebrations, in Tajikistan. At the centre of my analysis is the figure of a state employed ‘worker of culture’, Farkhod, whose family was affected by recent, rather militant, attempts by the state to regulate tradition. By following the story of his daughter’s wedding, I analyse how Farkhod tries to reconcile his roles of a caring father, a respectful community member, and a law-abiding citizen. I argue that the tanzim exacerbates the mismatch between the government’s attempts to impose a rigid notion of tradition and promote the idea of a certain kind of modern citizen, and people’s own understandings of being a modern and moral person having a good wedding.",
+				"issue": "2",
+				"language": "en",
+				"libraryCatalog": "brill.com",
+				"pages": "147-171",
+				"publicationTitle": "Oriente Moderno",
+				"shortTitle": "‘Our Traditions Will Kill Us!’",
+				"url": "https://brill.com/view/journals/ormo/100/2/article-p147_2.xml",
+				"volume": "100",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Tajikistan"
+					},
+					{
+						"tag": "law"
+					},
+					{
+						"tag": "marriage"
+					},
+					{
+						"tag": "modernity"
+					},
+					{
+						"tag": "tradition"
+					}
+				],
+				"notes": [
+					"LF:"
+				],
+				"seeAlso": []
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "https://brill.com/view/journals/ormo/100/2/article-p172_3.xml",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"title": "Alignment and Alienation: The Ambivalent Modernisations of Uyghur Marriage in the 21st Century",
+				"creators": [
+					{
+						"firstName": "Rune",
+						"lastName": "Steenberg",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "",
+						"lastName": "Musapir",
+						"creatorType": "author"
+					}
+				],
+				"date": "2020",
+				"DOI": "10.1163/22138617-12340247",
+				"ISSN": "2213-8617, 0030-5472",
+				"abstractNote": "Abstract Uyghur marriages in Xinjiang in the 2010s have been characterised by various, sometimes seemingly contradictory trends of modernisation, such as monetisation, simplification, emphasis on ethnic symbolism, displays of piety and the active integration of both Turkish, Western and Chinese elements. This article views these trends as complex, inter-related reactions to the region’s socio-economic transformations and political campaigns. It analyses how these transformations and campaigns affect everyday decisions at the local level. The study of marriage provides a good insight into the effects of economic and political transformations on the ground. In such studies, we argue for a distinction between trends on the level of symbolic positioning and identity display from trends on a deeper structural level pertaining to social relations, economic integration and household strategies. In the case of Uyghurs in southern Xinjiang these two levels have shown opposite trends. On a surface level of symbolic display, the relatively open years of 2010-2014 allowed for the flourishing of trends that did not follow the Party-State line, such as Islamic piety and a strengthened Uyghur ethno-national identity. Yet, on a deeper structural level these trends signified improved integration into modern Chinese society. In contrast, the increased state violence of 2015-2020 enforced a strong symbolic alignment with Chinese Communist Party (CCP) ideology but at the same time alienated the Uyghur population from this society effectively necessitating the development of forms of organisation that the CCP deems backwards and undesirable.",
+				"issue": "2",
+				"language": "en",
+				"libraryCatalog": "brill.com",
+				"pages": "172-199",
+				"publicationTitle": "Oriente Moderno",
+				"shortTitle": "Alignment and Alienation",
+				"url": "https://brill.com/view/journals/ormo/100/2/article-p172_3.xml",
+				"volume": "100",
+				"attachments": [
+					{
+						"title": "Full Text PDF",
+						"mimeType": "application/pdf"
+					},
+					{
+						"title": "Snapshot",
+						"mimeType": "text/html"
+					}
+				],
+				"tags": [
+					{
+						"tag": "Uyghur"
+					},
+					{
+						"tag": "Xinjiang"
+					},
+					{
+						"tag": "commercialisation"
+					},
+					{
+						"tag": "marriage"
+					},
+					{
+						"tag": "modernisation"
+					}
+				],
+				"notes": [],
+				"seeAlso": []
+			}
+		]
+	}
+]
+/** END TEST CASES **/
