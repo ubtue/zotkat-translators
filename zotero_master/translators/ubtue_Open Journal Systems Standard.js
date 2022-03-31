9781e713fda6e9e3bc5af162b8d1f324082c6129
socheres <kim.changhe@googlemail.com>
2022-03-31 16:34:47 +0200

    set priority for pkp and ojs standard https://github.com/ubtue/DatenProbleme/issues/1660 https://github.com/ubtue/DatenProbleme/issues/1693

--- zotero_master/translators/ubtue_Open Journal Systems Standard.js
+++ zotero_master/translators/ubtue_Open Journal Systems Standard.js
@@ -5,11 +5,11 @@
 	"target": "\\/(article|issue)\\/(view)?",
 	"minVersion": "3.0",
 	"maxVersion": "",
-	"priority": 99,
+	"priority": 98,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-03-17 10:30:53"
+	"lastUpdated": "2022-03-31 16:30:53"
 }
 
 /*
--- zotero_master/translators/ubtue_PKP Catalog Systems.js
+++ zotero_master/translators/ubtue_PKP Catalog Systems.js
@@ -5,11 +5,11 @@
 	"target": "/(article|preprint|issue)/view/|/catalog/book/|/search/search|/index\\.php/default",
 	"minVersion": "2.1.9",
 	"maxVersion": "",
-	"priority": 98,
+	"priority": 99,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-03-28 14:25:10"
+	"lastUpdated": "2022-03-31 16:25:10"
 }
 
 /*
