a05514c194f102cbaf113987a3c2ff88b17f81f4
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-06-08 17:52:24 +0200

    ubtue/DatenProbleme/issues/1772

--- ubtue_Journal of Religion and Society.js
+++ ubtue_Journal of Religion and Society.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-11-17 14:46:21"
+	"lastUpdated": "2022-06-08 15:51:33"
 }
 
 /*
@@ -86,12 +86,14 @@ function scrape(doc, url) {
 		if (itemVolume && itemVolume.match(/^\d+/)) item.volume = itemVolume;
 		let abstractKeywordsEntry = ZU.xpathText(doc, '//meta[@name="DCTERMS.abstract"]/@content');
 		if (abstractKeywordsEntry && abstractKeywordsEntry !== null) item.abstractNote = abstractKeywordsEntry.split('|Keywords: ')[0];
-		if (abstractKeywordsEntry && abstractKeywordsEntry !== null) {
+		if (abstractKeywordsEntry !== null) {
+			if (abstractKeywordsEntry.match(/\|Keywords: /) != null) {
 			let keywords = abstractKeywordsEntry.split('|Keywords: ')[1].split(',');
 			if (keywords && keywords !== null) {
 				for (let k of keywords) {
 					item.tags.push(k.trim().replace(/^\w/gi,function(m){ return m.toUpperCase();}));
 				}	
+			}
 			}		
 		}
 		//search and remove keyword "Journal Article" 
@@ -104,6 +106,7 @@ function scrape(doc, url) {
 		trans.doWeb(doc, url);
 	});
 }
+
 
 
 
@@ -127,6 +130,11 @@ var testCases = [
 	{
 		"type": "web",
 		"url": "http://moses.creighton.edu/JRS/toc/2021.html",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://moses.creighton.edu/JRS/toc/2016.html",
 		"items": "multiple"
 	}
 ]
