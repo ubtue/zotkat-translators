666f9c522d53c3187715383af9eee6550f29c930
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-11-21 08:41:25 +0100

    ubtue/DatenProbleme/issues/1835

--- ubtue_Open Journal Systems Standard.js
+++ ubtue_Open Journal Systems Standard.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-11-07 10:10:06"
+	"lastUpdated": "2022-11-21 07:26:20"
 }
 
 /*
@@ -38,7 +38,7 @@ function detectWeb(doc, url) {
 function getSearchResults(doc, url) {
 	var items = {};
 	var found = false;
-	var rows = doc.querySelectorAll('.title a[href*="/view/"], .article__title a[href*="/view/"], .summary_title, .title a[href*="/catalog/"], \
+	var rows = doc.querySelectorAll('.title a[href*="/view/"], .article-summary-title a[href*="/view/"], .article__title a[href*="/view/"], .summary_title, .title a[href*="/catalog/"], \
 		.tocTitle a[href*="/view/"], .tocArticleTitle a[href*="/view/"], .tocTitle a[href*="/catalog/"], .media-heading a[href*="/view/"]');
 	if (rows.length == 0 && url.match(/(otwsa-otssa)|(koersjournal)/)) {
 		rows = ZU.xpath(doc, '//div[@class="article-summary-title"]//a');
