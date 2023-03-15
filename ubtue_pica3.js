37dbc14ec9fc28840fdaf03ee6fac15161a54213
socheres <kim.changhe@googlemail.com>
2023-03-15 14:56:14 +0100

    activate ZU.cleannISSN

--- ubtue_pica3.js
+++ ubtue_pica3.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 2,
 	"browserSupport": "gcs",
-	"lastUpdated": "2022-03-08 13:35:00"
+	"lastUpdated": "2022-03-15 14:55:00"
 }
 
 // Zotero Export Translator in Pica3 Format für das Einzeln- und Mulitiupload in WinIBW
@@ -311,7 +311,7 @@ function performExport() {
 		var retrieve_sign = "";
 		if (!item.ISSN)
 				item.ISSN = "";
-		//if (item.ISSN.substring(0,4) != "IXTH") item.ISSN = ZU.cleanISSN(item.ISSN);
+		if (item.ISSN.substring(0,4) != "IXTH") item.ISSN = ZU.cleanISSN(item.ISSN);
 		//enrich items based on their ISSN
 		if (issn_to_language_code.get(item.ISSN) !== undefined) {
 			item.language = issn_to_language_code.get(item.ISSN);
