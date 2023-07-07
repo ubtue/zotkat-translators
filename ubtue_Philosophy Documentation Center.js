42266d6ac4948b02eba9300f6a9dc53c646e4ade
Paula Hähndel <paula.haehndel@uni-tuebingen.de>
2023-07-07 11:36:53 +0200

    changed PDC-translator ubtue/DatenProbleme#1934

--- ubtue_Philosophy Documentation Center.js
+++ ubtue_Philosophy Documentation Center.js
@@ -2,14 +2,14 @@
 	"translatorID": "5e3f67c9-f4e5-4dc6-ad9a-93bf263a585a",
 	"label": "ubtue_Philosophy Documentation Center",
 	"creator": "Madeesh Kannan",
-	"target": "^https://www.pdcnet.org/",
+	"target": "pdcnet.org",
 	"minVersion": "3.0",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-11-03 16:21:26"
+	"lastUpdated": "2023-07-07 09:24:46"
 }
 
 /*
@@ -34,16 +34,16 @@
 */
 
 function detectWeb(doc, url) {
-  if (url.includes('/jrv/')) {
+  if (url.includes('/jrv/') || url.includes('/content/')) {
 	return "JournalArticle";
   } else
-  if (url.includes('/collection-anonymous/')) {
+  if (url.includes('/collection')) {
 	return "multiple";
   }
   return false;
 }
 
-function getSearchResults(doc) {
+function getSearchResults(doc, url) {
 	var items = {};
 	var found = false;
 	var rows = ZU.xpath(doc, '//*[contains(concat( " ", @class, " " ), concat( " ", "purchase", " " ))]');
@@ -55,12 +55,30 @@ function getSearchResults(doc) {
 		found = true;
 		items[href] = title;
 	}
+	if (!found) {
+		//rows = ZU.xpath(doc, '//tr[td[a[@class="rl_anchor"]]]');
+		rows = ZU.xpath(doc, '//a[@class="rl_anchor"]');
+		titles = ZU.xpathText(doc, '//span[@class="title"]/label').trim().split('\n');
+		if (rows[0]) {
+			let name = rows[0].name.match(/[^_]+/)[0];
+			for (let i=0; i<rows.length; i++) {
+				//let row = rows[i].innerHTML;
+				let row = rows[i].name;
+				//let href = "https://www.pdcnet.org/collection/"+row.match(/a href="(show[^"]+)/)[1];
+				let href = "https://www.pdcnet.org/"+name+"/content/"+row;
+				let title = titles[i].trim();
+				if (!href || !title) continue;
+				found = true;
+				items[href] = title;
+			}
+		}
+	}
 	return found ? items : false;
 }
 
 function doWeb(doc, url) {
   if (detectWeb(doc, url) == "multiple") {
-	Zotero.selectItems(getSearchResults(doc, false), function (items) {
+	Zotero.selectItems(getSearchResults(doc, url, false), function (items) {
 	  if (items) ZU.processDocuments(Object.keys(items), scrape);
 	});
   } else
@@ -69,7 +87,7 @@ function doWeb(doc, url) {
   }
 }
 
-function scrape(doc) {
+function scrape(doc, url) {Z.debug(url);
 	var translator = Zotero.loadTranslator("web");
 	translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48");
 	translator.setDocument(doc);
@@ -81,6 +99,7 @@ function scrape(doc) {
 		if(issnEntry && issnEntry.match(/Print_ISSN/i) && issnEntry.match(/Print_ISSN=(\d{4}-\d{3}[\dx])/i)) i.ISSN = issnEntry.match(/Print_ISSN=(\d{4}-\d{3}[\dx])/i)[1];
 		if(issnEntry && issnEntry.match(/Book Review/i)) i.tags.push('RezensionstagPica');
 		i.attachments = [];
+		//i.url = url;
 		i.complete();
 	});
 	translator.translate();
@@ -104,7 +123,7 @@ var testCases = [
 				],
 				"date": "2019/10/01",
 				"DOI": "10.5840/philotheos20191928",
-				"ISSN": "2620-0163",
+				"ISSN": "1451-3455",
 				"abstractNote": "In this article, following the introductory chapters of the Platonic dialogue Charmides (153a1-154b7), we attempt to investigate the terms of transition from a simple dialogue to dialectics. Interpreting the expressive means used, we attempt to explain how Plato goes from historicity to systematicity, in order to create the appropriate conditions to build a definition about a fundamental virtue as well as to set the criteria to be followed in a philosophical debate. Our study is divided in two sections, each of which is also divided in two subsections. In the first section, we investigate the historical context of the dialogue and the terms of transition from a single dialogue to dialectics. In the second section, we attempt to define according to Socrates’ judgments the mental and moral quality of the young men as well as the terms and conditions of the right interlocutor. At the end of each section, we present a table of concepts to bring to light the conceptual structures that Plato builds, which reveal the philosophical development in this dialogue.",
 				"issue": "2",
 				"language": "en",
@@ -113,16 +132,7 @@ var testCases = [
 				"publicationTitle": "Philotheos",
 				"url": "https://www.pdcnet.org/pdc/bvdb.nsf/purchase?openform&fp=philotheos&id=philotheos_2019_0019_0002_0149_0165",
 				"volume": "19",
-				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Snapshot",
-						"mimeType": "text/html"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -145,7 +155,7 @@ var testCases = [
 				],
 				"date": "2021/04/30",
 				"DOI": "10.5840/acpq202147222",
-				"ISSN": "2153-8441",
+				"ISSN": "1051-3558",
 				"abstractNote": "This paper outlines the views of two 17th century thinkers (Thomas Jackson and John Locke) on the question of the metaphysics of resurrection. I show that Jackson and Locke each depart from central 17th century Scholastic convictions regarding resurrection and philosophical anthropology (convictions laid out in section II). Each holds that matter or material continuity is not a plausible principle of diachronic individuation for living bodies such as human beings. Despite their rejection of the traditional view, they each provide a defence of the possibility of a personal afterlife. I outline these (quite different) defences in sections III–IV. I then argue (section V) that it is likely either that Locke had read Jackson on the issue of resurrection or that the two were influenced by a common source. I argue that matter might provide a suitable principle of diachronic individuation in both everyday cases of living bodies and in the case of resurrection.",
 				"issue": "2",
 				"language": "en",
@@ -154,16 +164,7 @@ var testCases = [
 				"publicationTitle": "American Catholic Philosophical Quarterly",
 				"url": "https://www.pdcnet.org/pdc/bvdb.nsf/purchase?openform&fp=acpq&id=acpq_2021_0095_0002_0165_0194",
 				"volume": "95",
-				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Snapshot",
-						"mimeType": "text/html"
-					}
-				],
+				"attachments": [],
 				"tags": [],
 				"notes": [],
 				"seeAlso": []
@@ -186,7 +187,7 @@ var testCases = [
 				],
 				"date": "2021/02/23",
 				"DOI": "10.5840/acpq20219516",
-				"ISSN": "2153-8441",
+				"ISSN": "1051-3558",
 				"issue": "1",
 				"language": "en",
 				"libraryCatalog": "www.pdcnet.org",
@@ -195,16 +196,7 @@ var testCases = [
 				"shortTitle": "John Henry Newman on Truth and its Counterfeits",
 				"url": "https://www.pdcnet.org/pdc/bvdb.nsf/purchase?openform&fp=acpq&id=acpq_2021_0095_0001_0155_0160",
 				"volume": "95",
-				"attachments": [
-					{
-						"title": "Full Text PDF",
-						"mimeType": "application/pdf"
-					},
-					{
-						"title": "Snapshot",
-						"mimeType": "text/html"
-					}
-				],
+				"attachments": [],
 				"tags": [
 					{
 						"tag": "RezensionstagPica"
@@ -214,6 +206,16 @@ var testCases = [
 				"seeAlso": []
 			}
 		]
+	},
+	{
+		"type": "web",
+		"url": "https://www.pdcnet.org/collection/browse?fp=agstm&fq=agstm%2FVolume%2F8938%7C62%2F8998%7CIssue%5C%5C%3A+2%2F&rows=100&sort=year+asc%2Cvolume+asc%2Cissue+asc%2Cpagenumber_segment+asc%2Cpagenumber_first+asc",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "https://www.pdcnet.org/collection-anonymous/browse?fp=agstm&fq=agstm/Volume/8937%7C63/8999%7CIssue:%201/",
+		"items": "multiple"
 	}
 ]
 /** END TEST CASES **/
