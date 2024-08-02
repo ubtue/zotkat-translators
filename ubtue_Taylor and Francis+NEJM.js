1c6af5ade30a0077ce9fc02fe0b7161105fd6467
mara-spiess <mara.spiess@uni-tuebingen.de>
2024-08-02 15:16:20 +0200

    addresses https://github.com/ubtue/DatenProbleme/issues/2141 and https://github.com/ubtue/DatenProbleme/issues/2146

--- ubtue_Taylor and Francis+NEJM.js
+++ ubtue_Taylor and Francis+NEJM.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2023-08-09 08:15:46"
+	"lastUpdated": "2024-08-02 09:31:01"
 }
 
 /*
@@ -201,10 +201,12 @@ function finalizeItem(item, doc, doi, baseUrl) {
 	// mark articles as "LF" (MARC=856 |z|kostenfrei), that are published as open access
 	let AccessIconLocation = doc.querySelector('.accessIconLocation[alt]');
 	if (AccessIconLocation && AccessIconLocation.alt.match(/open\s+access/gi)) item.notes.push('LF:');
-	else if (ZU.xpath(doc, '//span[contains(@class, "access-icon") and contains(@class, "oa")]').length != 0) item.notes.push('LF:');
+	else if (doc.querySelectorAll('.access-icon.full').length !== 0) item.notes.push();
+	else if (doc.querySelectorAll('.access-icon.oa').length !== 0) item.notes.push('LF:');
 	addArticleNumber(doc, item);
 	item.complete();
 }
+
 
 
 
