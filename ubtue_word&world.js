a234000d055a2abdc3a0830800ab424ca27c183c
hlindeboom <hjordis.lindeboom@uni-tuebingen.de>
2025-04-16 10:21:52 +0200

    Updated Word & World Translator

--- ubtue_word&world.js
+++ ubtue_word&world.js
@@ -2,20 +2,20 @@
 	"translatorID": "cc9857c7-879b-4424-84ab-305d4f41047c",
 	"label": "ubtue_word&world",
 	"creator": "Helena Nebel",
-	"target": "wordandworld.luthersem.edu/issues.aspx\\?(article|issue)_id=",
+	"target": "^https?://wordandworld\\.luthersem\\.edu/issue/",
 	"minVersion": "3.0.4",
 	"maxVersion": "",
 	"priority": 100,
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2022-08-16 13:35:33"
+	"lastUpdated": "2025-04-16 08:16:13"
 }
 
 /*
 	***** BEGIN LICENSE BLOCK *****
 
-	Copyright © 2019 Universitätsbibliothek Tübingen.  All rights reserved.
+	Copyright © 2025 Universitätsbibliothek Tübingen.  All rights reserved.
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as published by
@@ -33,81 +33,70 @@
 	***** END LICENSE BLOCK *****
 */
 
-function detectWeb(doc, url) {
-	if (url.match(/\?issue_id=/) && getSearchResults(doc, true)) {
-		return "multiple";
-	} 
-	else if (url.match(/\?article_id=/)) return "journalArticle";
-	else return false;
+function detectWeb(doc) {
+	return doc.querySelector('a.article-name[href$=".pdf"]') ? 'multiple' : false;
 }
 
-function getSearchResults(doc, checkOnly) {
-  let items = {};
-  let found = false;
-  
-  rows = ZU.xpath(doc, '//h5/a[contains(@href, "article_id=")]');
-  for (let row of rows) {
-	if (row.textContent != "PDF") {
-	let href = ZU.xpathText(row, './@href');
-	let title = row.textContent;
-	if (!href || !title) continue;
-	if (checkOnly) return true;
-	found = true;
-	items[href] = title;
-  }
-  }
-  return found ? items : false;
+function extractYearIssue(doc) {
+	let heading = doc.querySelector('h3.ww-issue-year');
+	if (!heading) return null;
+
+	let text = ZU.trimInternal(heading.textContent);
+	let match = text.match(/(Spring|Summer|Autumn|Fall|Winter)\s+(\d{4})/i);
+	if (!match) return null;
+
+	let season = match[1].toLowerCase();
+	let year = parseInt(match[2]);
+	let issue = { spring: 1, summer: 2, autumn: 3, winter: 4 }[season];
+
+	let month = { spring: 5, summer: 8, autumn: 11, winter: 2 }[season];
+	let day = 30;
+
+	let formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+	
+	return { year: year.toString(), issue: issue.toString(), date: formattedDate };
 }
 
-function getArticle(doc, url) {
-	let infoTag = ZU.xpath(doc, '//div[@id="ww_ai_wrap"]')[0];
-	item = new Zotero.Item('journalArticle');
-	item.tags = [];
-	item.title = ZU.xpathText(infoTag, './h2');
-	for (let authorTag of ZU.xpath(infoTag, './/a[@class="vtip"]')) {
-		item.creators.push(ZU.cleanAuthor(authorTag.textContent, "author"));
-	}
-	let issueInfo = ZU.xpathText(infoTag, './/a[contains(@href, "issue_id")]');
-	if (issueInfo.match(/Vol\.?\s*(\d+).+No.?\s*(\d+).+(\d{4})/i)) {
-		item.volume = issueInfo.match(/Vol\.?\s*(\d+).+No.?\s*(\d+).+(\d{4})/i)[1];
-		item.issue = issueInfo.match(/Vol\.?\s*(\d+).+No.?\s*(\d+).+(\d{4})/i)[2];
-		item.date = issueInfo.match(/Vol\.?\s*(\d+).+No.?\s*(\d+).+(\d{4})/i)[3];
-		item.url = url;
-		if (infoTag.textContent.match(/\d{4}\)(.+?)Download\s+Article\s+PDF/)) {
-			item.abstractNote = infoTag.textContent.match(/\d{4}\)(.+?)Download\s+Article\s+PDF/)[1];
-			if (item.abstractNote.match(/^Download\s+Article\s+PDF/)) item.abstractNote = "";
-		}
-		item.publicationTitle = "Word & World : Theology for Christian Ministry";
-		item.notes.push('LF:');
-		item.ISSN = "0275-5270";
-		if (infoTag.textContent.match(/Article\s*Type:?\s+Review/i)) {
-			item.tags.push('RezensionstagPica');
+async function doWeb(doc, url) {
+	let items = {};
+	let links = doc.querySelectorAll('a.article-name[href$=".pdf"]');
+
+	for (let link of links) {
+		let href = link.href;
+		let title = link.querySelector('h3');
+		if (href && title) {
+			items[href] = ZU.trimInternal(title.textContent);
 		}
 	}
-	item.complete();
-}
 
-function doWeb(doc, url) {
-	if (detectWeb(doc, url) === "multiple") {
-		Zotero.selectItems(getSearchResults(doc), function (items) {
-			if (!items) {
-				return true;
-			}
-			var articles = [];
-			for (var i in items) {
-				articles.push(i);
-			}
-			ZU.processDocuments(articles, getArticle);
+	let selected = await Zotero.selectItems(items);
+	if (!selected) return;
+
+	let issueMeta = extractYearIssue(doc);
+
+	for (let pdfUrl in selected) {
+		let item = new Zotero.Item('journalArticle');
+		item.title = selected[pdfUrl];
+		item.url = pdfUrl;
+		item.libraryCatalog = "Word & World";
+		item.language = "eng";
+
+		if (issueMeta) {
+			item.date = issueMeta.date;
+			item.issue = issueMeta.issue;
+		}
+
+		item.attachments.push({
+			title: "Full Text PDF",
+			mimeType: "application/pdf",
+			url: pdfUrl
 		});
-	} else
-		getArticle(doc, url);
+
+		item.complete();
+	}
 }
+
 /** BEGIN TEST CASES **/
 var testCases = [
-	{
-		"type": "web",
-		"url": "https://wordandworld.luthersem.edu/issues.aspx?issue_id=166",
-		"items": "multiple"
-	}
 ]
 /** END TEST CASES **/
