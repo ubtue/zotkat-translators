3527a3fcfdf50b90b6560f6cfa5a4f9333c914d5
mara-spiess <mara.spiess@uni-tuebingen.de>
2025-06-13 14:45:41 +0200

    adds RezensionstagPica, addresses https://github.com/ubtue/DatenProbleme/issues/2324

--- ubtue_Open Journal Systems Standard.js
+++ ubtue_Open Journal Systems Standard.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2025-06-13 12:38:35"
+	"lastUpdated": "2025-06-13 12:42:36"
 }
 
 /*
@@ -476,9 +476,9 @@ function invokeEMTranslator(doc) {
 		if (["2159-6875"].includes(i.ISSN)) {
 			if (reviewURLs.includes(i.url)) i.tags.push("RezensionstagPica");
 		}
-		if (['2617-3697', '2660-4418', '2748-6419', '1988-3269', '1804-6444', '2391-4327', '2174-0887', '2709-8435', '2296-469X', '2057-5831', '2695-4397', '2169-2289'].includes(i.ISSN)) {
+		if (['2617-3697', '2660-4418', '2748-6419', '1988-3269', '1804-6444', '2391-4327', '2174-0887', '2709-8435', '2296-469X', '2057-5831', '2695-4397', '2169-2289', '2179-0019'].includes(i.ISSN)) {
 			let articleType = ZU.xpathText(doc, '//meta[@name="DC.Type.articleType"]/@content');
-			if (articleType && articleType.match(/Media reviews|Rezensionen|recensiones|Reseñas|(?:Part\s+Two:\s+)?Reviews?|Buchbesprechungen/i)) {
+			if (articleType && articleType.match(/Media reviews|Rezensionen|recensiones|Reseñas|resenhas|(?:Part\s+Two:\s+)?Reviews?|Buchbesprechungen/i)) {
 				i.tags.push("RezensionstagPica");
 			}
 		}
