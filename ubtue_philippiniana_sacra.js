2b3ecd3f468a0ad8c86314dccf58f784ac34496f
hlindeboom <hjordis.lindeboom@uni-tuebingen.de>
2026-08-03 15:56:03 +0200

    added page and affiliation extraction ubtue/DatenProbleme#2425

--- ubtue_philippiniana_sacra.js
+++ ubtue_philippiniana_sacra.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2026-05-08 12:52:23"
+	"lastUpdated": "2026-08-03 08:37:40"
 }
 
 /*
@@ -87,80 +87,128 @@ function romanToInt(roman) {
 	return value;
 }
 
-function doWeb(doc, url) {
-	let items = getSearchResults(doc, false);
+async function doWeb(doc, url) {
+	const items = await Zotero.selectItems(getSearchResults(doc, false));
 	if (!items) return;
 
-	Zotero.selectItems(items, function (selected) {
-		if (!selected) return;
+	for (let block of doc.querySelectorAll("div.main-content-item")) {
+		let pdfURL = block.querySelector("h5 a")?.href;
 
-		let blocks = doc.querySelectorAll("div.main-content-item");
-
-		for (let block of blocks) {
-			let titleEl = block.querySelector("h5 a");
-			if (!titleEl) continue;
-
-			let pdfURL = titleEl.href;
-			if (!(pdfURL in selected)) continue;
-
-			let item = new Zotero.Item("journalArticle");
-			let isReview = false;
-			let title = titleEl.textContent.trim().replace(/\s+/g, " ");
-			if (title.toLowerCase().includes("review")) isReview = true;
-			title = title.replace(/\s*\(PHILIPPINIANA\s*RECORDS\)\s*/i, "").replace(/\s*\(REVIEWS\s*&\s*NOTICES\)\s*/i, "").trim();
-			item.title = ZU.capitalizeTitle(title, true);
-			let doiEl = block.querySelector('a[href^="https://doi.org"], a[href^="http://doi.org"]');
-			if (doiEl) {
-				item.DOI = doiEl.textContent.trim();
-			}
-			const AFFILIATIONS = ["O\\.?P\\.?", "O\\.?S\\.?B\\.?", "O\\.?F\\.?M\\.?", "S\\.?D\\.?B\\.?", "J\\.?C\\.?D\\.?"];
-			const affiliationRegex = new RegExp("\\b(" + AFFILIATIONS.join("|") + ")\\b", "gi");
-			let authorDD = block.querySelector("dt i.fa-users")?.parentElement?.nextElementSibling;
-			if (authorDD) {
-				let authors = authorDD.textContent.split(/,\s*/);
-				for (let name of authors) {
-					//replace OP affiliation or role, e.g. (translator)
-					name = name.replace(affiliationRegex, "").replace(/\(.*?\)/g, "").replace(/\s+/g, " ").trim();
-					if (!name) continue;
-					item.creators.push(Zotero.Utilities.cleanAuthor(ZU.capitalizeTitle(name, true), "author"));
-				}
-			}
-			let abstractP = block.querySelector("p.text-justify");
-			if (abstractP) {
-				let abs = abstractP.textContent.trim().replace(/^Abstract\s*:/i, "");
-				item.abstractNote = abs.trim();
-			}
-			let kwP = block.querySelector("p.text-muted span.font-weight-bold");
-			if (kwP && kwP.textContent.includes("Keywords")) {
-				let kwText = kwP.parentElement.textContent.replace(/Keywords\s*:/i, "");
-				item.tags = kwText.split(",").map(t => t.trim()).filter(Boolean);
-			}
-			if (isReview) item.tags.push('RezensionstagPica');
-			if (pdfURL) {
-				item.attachments.push({
-					url: pdfURL,
-					title: "Full Text PDF",
-					mimeType: "application/pdf"
-				});
-			}
-			let header = doc.querySelector("#headerissue")?.textContent || "";
-			header = header.replace(/\s+/g, " ").trim();
-			let volumeMatch = header.match(/Vol\.?\s+([A-Z0-9]+)/i);
-			let issueMatch  = header.match(/Issue\s+No\.?\s+(\d+)/i);
-			let yearMatch   = header.match(/(\d{4})\s*$/);
-			let volume = volumeMatch ? volumeMatch[1] : "";
-			item.volume = romanToInt(volume);
-			item.issue  = issueMatch ? issueMatch[1] : "";
-			item.date   = yearMatch ? yearMatch[1] : "";
-
-			item.publicationTitle = "Philippiniana Sacra";
-			item.url = pdfURL;
-			item.ISSN = "2651-7418";
-			item.language = "eng";
-
-			item.complete();
+		if (pdfURL && items[pdfURL]) {
+			await scrape(block, doc);
 		}
-	});
+	}
+}
+
+async function scrape(block, doc) {
+	let item = new Zotero.Item("journalArticle");
+	let titleEl = block.querySelector("h5 a");
+	let pdfURL = titleEl.href;
+	let isReview = false;
+	let title = titleEl.textContent.trim().replace(/\s+/g, " ");
+	if (title.toLowerCase().includes("review")) {
+		isReview = true;
+	}
+	title = title
+		.replace(/\s*\(PHILIPPINIANA\s*RECORDS\)\s*/i, "")
+		.replace(/\s*\(REVIEWS\s*&\s*NOTICES\)\s*/i, "")
+		.trim();
+	item.title = ZU.capitalizeTitle(title, true);
+
+	let doiEl = block.querySelector(
+		'a[href^="https://doi.org"], a[href^="http://doi.org"]'
+	);
+	if (doiEl) {
+		item.DOI = doiEl.textContent.trim();
+	}
+
+	const AFFILIATIONS = [
+		"O\\.?P\\.?",
+		"O\\.?S\\.?B\\.?",
+		"O\\.?F\\.?M\\.?",
+		"S\\.?D\\.?B\\.?",
+		"J\\.?C\\.?D\\.?",
+		"S\\.?V\\.?D\\.?",
+		"O\\.?S\\.?A\\.?",
+		"Ph\\.D\\.?",
+		"S\\.?J\\.?",
+		"Jr\\.?"
+	];
+
+	const affiliationRegex = new RegExp("\\b(" + AFFILIATIONS.join("|") + ")\\b", "gi");
+
+	let authorDD = block.querySelector("dt i.fa-users")?.parentElement?.nextElementSibling;
+	if (authorDD) {
+		let authors = authorDD.textContent.split(/\sand\s/);
+		for (let name of authors) {
+			let affiliations = [];
+			name = name.replace(affiliationRegex, match => {
+				affiliations.push(match.replace(/\s+/g, ""));
+				return "";
+			});
+			name = name.replace(/\(.*?\)/g, "").replace(/\s+/g, " ").replace(/†/g, "").trim();
+			if (!name) continue;
+			let author = ZU.cleanAuthor(ZU.capitalizeTitle(name, true),	"author");
+			if (affiliations.length) {
+				author.lastName += " " + affiliations.join(" ");
+			}
+			item.creators.push(author);
+		}
+	}
+
+	let abstractP = block.querySelector("p.text-justify");
+	if (abstractP) {
+		item.abstractNote = abstractP.textContent.trim().replace(/^Abstract\s*:/i, "").trim();
+	}
+	let kwP = block.querySelector("p.text-muted span.font-weight-bold");
+	item.tags = [];
+	if (kwP && kwP.textContent.includes("Keywords")) {
+		let kwText = kwP.parentElement.textContent.replace(/Keywords\s*:/i, "");
+		item.tags.push(...kwText.split(",").map(t => t.trim()).filter(Boolean));
+	}
+
+	if (isReview) {
+		item.tags.push("RezensionstagPica");
+	}
+
+	let header = doc.querySelector("#headerissue")?.textContent || "";
+	header = header.replace(/\s+/g, " ").trim();
+
+	let volumeMatch = header.match(/Vol\.?\s+([A-Z0-9]+)/i);
+	let issueMatch = header.match(/Issue\s+No\.?\s+(\d+)/i);
+	let yearMatch = header.match(/(\d{4})\s/);
+	let volume = volumeMatch ? volumeMatch[1] : "";
+	item.volume = romanToInt(volume);
+	item.issue  = issueMatch ? issueMatch[1] : "";
+	item.date   = yearMatch ? yearMatch[1] : "";
+
+	// this requires the pdf-metadata-extractor to be running, see: https://github.com/ubtue/pdf-metadata-extractor
+	let extracted = {};
+	let site = 'philippiniana_sacra';
+	try {
+		const params = new URLSearchParams();
+		let pdfLink = titleEl.href;
+		params.append('url', pdfLink);
+		params.append('site', site);
+		extracted = await fetch(PYMUPDF_SERVER_ADDRESS, {
+				'method': "POST",
+				'headers' : { 'Content-Type': 'application/x-www-form-urlencoded' },
+				'body': params.toString()
+		}).then(response => response.json());
+
+	} catch (e) {
+		Zotero.debug("PDF extraction failed: " + e);
+	}
+
+	if (extracted.pages)
+		item.pages = extracted.pages;
+
+	item.publicationTitle = "Philippiniana Sacra";
+	item.url = pdfURL;
+	item.ISSN = "2651-7418";
+	item.language = "eng";
+
+	item.complete();
 }
 
 /** BEGIN TEST CASES **/
